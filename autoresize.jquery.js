ccdfbef367ff8c59746a5b1d4b685c1e3785f5f0
Yakovlev Alexander <ayakovlev@rambler.ru>
2013-11-18 11:24:21 +0400

    Some formatting.

--- autoresize.jquery.js
+++ autoresize.jquery.js
@@ -1,89 +1,92 @@
-/*
+/*!
  * jQuery autoResize (textarea auto-resizer)
  * @copyright James Padolsey http://james.padolsey.com
  * @version 1.04
  */
 
-(function($){
-    
-    $.fn.autoResize = function(options) {
-        
+(function ($) {
+
+    $.fn.autoResize = function (options) {
+
         // Just some abstracted details,
         // to make plugin users happy:
         var settings = $.extend({
-            onResize : function(){},
+            onResize : function () {},
             animate : true,
             animateDuration : 150,
-            animateCallback : function(){},
+            animateCallback : function () {},
             extraSpace : 20,
             limit: 1000,
             resizeImmediately: true
         }, options);
-        
+
         // Only textarea's auto-resize:
-        this.filter('textarea').each(function(){
-            
+        this.filter('textarea').each(function () {
+
                 // Get rid of scrollbars and disable WebKit resizing:
-            var textarea = $(this).css({resize:'none','overflow-y':'hidden'}),
-            
+            var textarea = $(this).css({resize: 'none', 'overflow-y': 'hidden'}),
+
                 // Cache original height, for use later:
                 origHeight = textarea.height(),
-                
+
                 // Need clone of textarea, hidden off screen:
-                clone = (function(){
-                    
+                clone = (function () {
+
                     // Properties which may effect space taken up by chracters:
-                    var props = ['height','width','lineHeight','textDecoration','letterSpacing'],
+                    var props = ['height', 'width', 'lineHeight', 'textDecoration', 'letterSpacing'],
                         propOb = {};
-                        
+
                     // Create object of styles to apply:
-                    $.each(props, function(i, prop){
+                    $.each(props, function (i, prop){
                         propOb[prop] = textarea.css(prop);
                     });
-                    
+
                     // Clone the actual textarea removing unique properties
                     // and insert before original textarea:
                     return textarea.clone().removeAttr('id').removeAttr('name').css({
                         position: 'absolute',
                         top: 0,
                         left: -9999
-                    }).css(propOb).attr('tabIndex','-1').insertBefore(textarea);
-					
+                    }).css(propOb).attr('tabIndex', '-1').insertBefore(textarea);
+
                 })(),
+
                 lastScrollTop = null,
-                updateSize = function(forceResize) {
-					
+
+                updateSize = function (forceResize) {
+
                     // Prepare the clone:
                     clone.height(0).val($(this).val()).scrollTop(10000);
-					
+
                     // Find the height of text:
                     var scrollTop = Math.max(clone.scrollTop(), origHeight) + settings.extraSpace,
                         toChange = $(this).add(clone);
                     scrollTop = Math.min(scrollTop, settings.limit);
-						
+
                     // Don't do anything if scrollTip hasen't changed:
                     if (lastScrollTop === scrollTop) { return; }
                     lastScrollTop = scrollTop;
-					
+
                     // Check for limit:
                     if (scrollTop == settings.limit) {
-                        $(this).css('overflow-y','');
+                        $(this).css('overflow-y', '');
                         if (!forceResize) {
                             return;
                         }
                     }
                     // Fire off callback:
                     settings.onResize.call(this);
-					
+
                     // Either animate or directly apply height:
                     settings.animate && textarea.css('display') === 'block' ?
-                        toChange.stop().animate({height:scrollTop}, settings.animateDuration, settings.animateCallback)
+                        toChange.stop().animate({height: scrollTop}, settings.animateDuration, settings.animateCallback)
                         : toChange.height(scrollTop);
                 };
+
                 if (settings.resizeImmediately) {
                     updateSize.call(this, true);
                 }
-            
+
             // Bind namespaced handlers to appropriate events:
             textarea
                 .unbind('.dynSiz')
@@ -96,14 +99,12 @@
                         updateSize.call(elem);
                     }, 100);
                 });
-            
+
         });
-        
+
         // Chain:
         return this;
-        
+
     };
-    
-    
-    
+
 })(jQuery);
